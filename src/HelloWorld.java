4520165283d530af4d9e37f6ddb4ff3d061cd504
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-02 12:08:36 -0500

    HW .java

---
+++ src/HelloWorld.java
@@ -0,0 +1,38 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println(" Hello, World!");
+        System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "value";
+        System.out.println(myString);
+
+        long myNumber = 123;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+// ======= this code wont run.  and is an example of what we can't do =============
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+//        =====================================
+        int x = 4;
+//        x = x + 5;
+        x += 5;
+
+        int z = 3;
+        int y = 4;
+//        y = y * z;
+        y *= z;
+    }
+}
