a0378c1dea31cae5c9aa7e53ad7556c064000a7b
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-03 12:43:45 -0500

    created console exercises and control flow exercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,218 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+	public static void main(String[] args) {
+
+//
+//        While
+//
+//        Create an integer variable i with a value of 5.
+//        Create a while loop that runs so long as i is less than or equal to 15
+//        Each loop iteration, output the current value of i, then increment i by one.
+//        Your output should look like this:
+//
+//        5 6 7 8 9 10 11 12 13 14 15
+// int i = 5;
+
+//while (i <= 15){
+//    System.out.println(i);
+//    i++;
+//        }
+
+//===================================
+//        Do While
+//
+//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+
+//int i = 0;
+//
+//do  {
+//    System.out.println(i);
+//    i+=2;
+//} while (i <= 100);
+
+//        Alter your loop to count backwards by 5's from 100 to -10.
+//int i = 100;
+//
+//do  {
+//    System.out.println(i);
+//    i-=5;
+//} while (i >= -10);
+
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+//
+//        2
+//        4
+//        16
+//        256
+//        65536
+//this didn't work with int because it wasn't the right range.
+//long i = 2;
+//
+//do  {
+//    System.out.println(i);
+//    i*=i;
+//} while (i <= 100000);
+//=================================================================
+//        For
+//
+//        Refactor the previous two exercises to use a for loop instead.
+
+//for (int i = 5;  i <= 15; i++){
+//    System.out.println(i);
+//}
+
+//for(int i = 0; i <= 100; i += 2){
+//    System.out.println(i);
+//}
+//
+//for (int i = 100; i >= -10; i -=5){
+//    System.out.println(i);
+//}
+
+//for (long i = 2; i <= 1000000; i *= i){
+//    System.out.println(i);
+//}
+//
+
+//        Fizzbuzz
+//
+//        One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+//
+//                Write a program that prints the numbers from 1 to 100. CHECK
+//        For multiples of three: print “Fizz” instead of the number.
+//        For the multiples of five: print “Buzz”.
+//        For numbers which are multiples of both three and five: print “FizzBuzz”.
+
+//for (int i = 1; i<= 100; i++){
+//    if (i % 3 == 0 && 1 % 5 == 0){
+//        System.out.println("Fizz Buzz");
+//    } else if (i % 5 == 0){
+//        System.out.println("Buzz");
+//    } else if ( i % 3 == 0) {
+//        System.out.println("Fizz");
+//    } else {
+//        System.out.println(i);
+//    }
+//}
+//======================================================
+//        Display a table of powers.
+//
+//                Prompt the user to enter an integer.
+//        Display a table of squares and cubes from 1 to the value entered.
+//        Ask if the user wants to continue.
+//        Assume that the user will enter valid data.
+//        Only continue if the user agrees to.
+//        Example Output
+//
+//
+//        What number would you like to go up to? 5
+//
+//        Here is your table!
+//
+//                number | squared | cubed
+//                ------ | ------- | -----
+//                1      | 1       | 1
+//                2      | 4       | 8
+//               3      | 9       | 27
+//                4      | 16      | 64
+//               5      | 25      | 125
+		Scanner sc = new Scanner(System.in);
+//        System.out.println("What number would you like to go up to? ");
+//        int userInput = sc.nextInt();
+//        System.out.println("Here is your table!");
+//        System.out.println("number | squared | cubed");
+//        System.out.println("------ | ------- | -----");
+//        for (int i = 1; i <= userInput; i++) {
+////            int square = i * i;
+////            int cubed =  i * (i * i);
+//            System.out.print(i);
+//            System.out.print(" | " + (i * i));
+//            System.out.print(" | " + (i * i * i) );
+//            System.out.println();
+//        }
+
+//        Convert given number grades into letter grades.
+//
+//                Prompt the user for a numerical grade from 0 to 100.
+//        Display the corresponding letter grade.
+//                Prompt the user to continue.
+//        Assume that the user will enter valid integers for the grades.
+//        The application should only continue if the user agrees to.
+//        Grade Ranges:
+//
+//        A : 100 - 88
+//        B : 87 - 80
+//        C : 79 - 67
+//        D : 66 - 60
+//        F : 59 - 0
+String answer = "";
+
+		System.out.println("Enter your grade 0-100");
+		int userInput = sc.nextInt();
+		System.out.println("A : 100 - 88");
+		System.out.println("B : 87 - 80");
+		System.out.println("C : 79 - 67");
+		System.out.println("D : 66 - 60");
+		System.out.println("F : 59 - 0");
+		System.out.println("Do you agree to these grade ranges: (Y/N)");
+	   answer = sc.next();
+   if (answer.equalsIgnoreCase("y") ){
+        String msg = "Your letter grade is: ";
+        if (userInput >= 88) {
+            System.out.println(msg + "A");
+        } else if (userInput <= 87 && userInput >= 80) {
+            System.out.println(msg + "B");
+        } else if (userInput <= 79 && userInput >= 67) {
+            System.out.println(msg + "C");
+        } else if (userInput <= 66 && userInput >= 60) {
+            System.out.println(msg + "D");
+        } else {
+            System.out.println(msg + "F");
+        }
+    }
+
+//		if (answer.equalsIgnoreCase("y") ) {
+//			int[] arrayForA = {99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88};
+//            int[] arrayForB = {87, 86, 85, 84, 83, 82, 81, 80};
+//            int[] arrayForC = {79, 78, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67};
+//            int[] arrayForD = {66, 65, 64, 63, 62, 61, 60};
+//			String msg = "Your letter grade is: ";
+//			int i = 0;
+//			boolean foundIt = false;
+//			for (i = 0; i < arrayForA.length; i++) {
+//                if (arrayForA[i] == userInput) {
+//                    System.out.println(msg + " A ");
+//                }
+//            }
+//                for (i = 0; i < arrayForB.length; i++) {
+//                    if (arrayForB[i] == userInput) {
+//                        System.out.println(msg + "B");
+//                    }
+//                }
+//                        for (i = 0; i < arrayForC.length; i++) {
+//                            if (arrayForC[i] == userInput) {
+//                                System.out.println(msg + "C");
+//                            }
+//                        }
+//                            for (i = 0; i < arrayForD.length; i++) {
+//                                if (arrayForD[i] == userInput) {
+//                                    System.out.println(msg + "D");
+//
+//                } else{
+//                    System.out.println(msg + "F");
+//                }
+//                }
+//          }
+
+
+
+
+
+
+
+
+//=========end==don't delete======================
+    }
+
+}
