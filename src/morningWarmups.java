71ba3cb578fc41cd153ff46546fda00c5c49036a
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-08 09:17:31 -0500

    created java morning warmups

--- src/morningWarmups.java
+++ src/morningWarmups.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class morningWarmups {
+    public static void main(String[] args) {
+
+
+        for (int i = 1; i<= 50; i++){
+            if (i % 5 == 0){
+                System.out.println(i);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        ===============================================================================
+    }
 }
