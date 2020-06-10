0923c51b3f009b5ab564707c3321e0de21dafb92
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-10 12:46:22 -0500

    created inputTest class for  oop exercise

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,4 +1,14 @@
 package util;
 
 public class InputTest {
+    public static void main(String[] args) {
+       Input user = new Input();
+       user.getString();
+       user.yesNo();
+       user.getInt(3, 15);
+       user.getInt();
+       user.getDouble(4.95, 100.50);
+       user.getDouble();
+    }
+
 }
