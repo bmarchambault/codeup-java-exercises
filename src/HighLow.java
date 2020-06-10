b985c642c86cf3a08ea99750db1a5700bfa88a2d
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-10 12:52:50 -0500

    working on highlow exercise

--- src/HighLow.java
+++ src/HighLow.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
-
-public class HighLow {
-    static Scanner sc = new Scanner(System.in).useDelimiter("\n");
-    public static void main(String[] args) {
-guessingGame();
-    }
+//import java.util.Scanner;
+//
+//public class HighLow {
+//    static Scanner sc = new Scanner(System.in).useDelimiter("\n");
+//    public static void main(String[] args) {
+//guessingGame();
+//    }
 
 //Game Development 101
 //
@@ -34,50 +34,50 @@ guessingGame();
 //    if userguess = limit; Sorry, you lose.
     // give rules:  you only get x amount of guesses.  if you guess it before you run out of guesses, you win.
 
-    public static void guessingGame() {
-        String response = "";
-        String userGuess;
-        int numOfUserGuesses = 0;
-        int guessLimit = 10;
-        int randomNum;
-        System.out.println("Would you like to play a guessing game? (y/n)");
-        response = sc.next();
-        if (response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yes")) {
-            System.out.println("You get 10 guesses.  I'll tell you higher or lower to help you.  If you guess before you run out of turns, you win.  Still want to play? (y/n)");
-            response = sc.next();
-            if (response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yes")) {
-                do{
-                randomNum = (int) (Math.random() * 100) + 1;
-                    System.out.println("Enter your guess:");
-                    userGuess = sc.next();
-                double userGuessdbl = Double.parseDouble(userGuess);
-                numOfUserGuesses += 1;
-
-//                for ( int numOfUserGuesses = 0; numOfUserGuesses < guessLimit; numOfUserGuesses ++) {
-            }while (numOfUserGuesses < guessLimit){
-                    if (userGuessdbl < randomNum) {
-                        System.out.println("Higher");
-                    } else if (userGuessdbl > randomNum) {
-                        System.out.println("Lower");
-                    } else if (userGuessdbl == randomNum) {
-                        System.out.println("GOOD GUESS");
-                    }else if(numOfUserGuesses == guessLimit ) {
-                        System.out.println("Sorry, no more guesses.  You loose.");
-                }
-                }
-            }else {
-                System.out.println("Maybe another time then.");
-            }
-        } else {
-            System.out.println("Maybe another time then.");
-        }
-
-    }
-
-
-
-
-
-
-
-}
+//    public static void guessingGame() {
+//        String response = "";
+//        String userGuess;
+//        int numOfUserGuesses = 0;
+//        int guessLimit = 10;
+//        int randomNum;
+//        System.out.println("Would you like to play a guessing game? (y/n)");
+//        response = sc.next();
+//        if (response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yes")) {
+//            System.out.println("You get 10 guesses.  I'll tell you higher or lower to help you.  If you guess before you run out of turns, you win.  Still want to play? (y/n)");
+//            response = sc.next();
+//            if (response.equalsIgnoreCase("y") || response.equalsIgnoreCase("yes")) {
+//                do{
+//                randomNum = (int) (Math.random() * 100) + 1;
+//                    System.out.println("Enter your guess:");
+//                    userGuess = sc.next();
+//                double userGuessdbl = Double.parseDouble(userGuess);
+//                numOfUserGuesses += 1;
+//
+////                for ( int numOfUserGuesses = 0; numOfUserGuesses < guessLimit; numOfUserGuesses ++) {
+//            }while (numOfUserGuesses < guessLimit){
+//                    if (userGuessdbl < randomNum) {
+//                        System.out.println("Higher");
+//                    } else if (userGuessdbl > randomNum) {
+//                        System.out.println("Lower");
+//                    } else if (userGuessdbl == randomNum) {
+//                        System.out.println("GOOD GUESS");
+//                    }else if(numOfUserGuesses == guessLimit ) {
+//                        System.out.println("Sorry, no more guesses.  You loose.");
+//                }
+//                }
+//            }else {
+//                System.out.println("Maybe another time then.");
+//            }
+//        } else {
+//            System.out.println("Maybe another time then.");
+//        }
+//
+//    }
+//
+//
+//
+//
+//
+//
+//
+//}
