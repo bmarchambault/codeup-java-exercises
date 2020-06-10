e3f9d8f27e40528ba6c1cf40969e58a9825f2635
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-10 12:52:19 -0500

    created dishtools class for  oop practice exercise

--- src/DishTools.java
+++ src/DishTools.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class DishTools {
+
+
 }
+//  PART 2
+//    Create a class of static fields (properties and methods) called DishTools
+//      - AVERAGE_COST_OF_DISH_IN_CENTS - an integer constant set to 13000
+//      - shoutDishName() - that takes in a Dish object and prints out the name in all caps
+//      - analyzeDishCost() - that takes in a Dish object and will print out either "More expensive than average" or "Less expensive than average", depending on the value of the dish costInCents compared to AVERAGE_COST_OF_DISH_IN_CENTS
+//      - flipRecommendation() - that takes in a Dish object and reverses the wouldRecommend boolean value
+//    Try out the DishTools methods with Dish objects in the DishTest main method
