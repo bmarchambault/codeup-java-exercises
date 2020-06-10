b6147885a3e1fbd360ca640065b2b07563b49834
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-10 12:44:30 -0500

    created dog class for oop lecture

--- src/oopLecture/Dog.java
+++ src/oopLecture/Dog.java
@@ -1,4 +1,18 @@
 package oopLecture;
 
 public class Dog {
+    public int age;
+    public String name;
+    public String breed;
+
+
+  //you can call this method in the main.
+    public void callForDog(String name){
+        if (name.equalsIgnoreCase(this.name)){
+            System.out.println(this.name + " is now walking towards you!");
+        }else{
+            System.out.println(this.name + " ignores you.");
+        }
+    }
+
 }
