b7b8c76bf77ea65667dcd43fe11224c5362c1c49
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-10 12:45:16 -0500

    created person class for  oop lecture

--- src/oopLecture/Person.java
+++ src/oopLecture/Person.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+package oopLecture;
+class Person {
+    //instance variables:
+    public String firstName;
+    public String lastName;
 
-public class Person {
+    public int age;
+    //this defines our object.
+//you can hard code values here.
+
+    //instance methods - the behaviors of the object:
+//    public String sayHello() {
+//
+//
+//    }
+
 }
