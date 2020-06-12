f0a4855c1e0acae0989e7fc448d061aebf8d7147
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-12 13:22:50 -0500

    created ArraysExercises

---
+++ src/ArraysExercises.java
@@ -0,0 +1,12 @@
+public class ArraysExercises {
+
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(numbers);
+//        returns reference type
+
+
+
+    }
+}
