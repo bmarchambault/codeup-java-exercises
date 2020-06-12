aa3789a4ea7d8662282d6583e3b9a1c8f4296792
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-12 13:22:10 -0500

    created CircleApp

---
+++ src/shapes/CircleApp.java
@@ -0,0 +1,12 @@
+package shapes;
+import util.Input;
+public class CircleApp {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+}
+//create a class named CircleApp that prompts the user for the radius of the circle using your Input class, creates a Circle object, and displays the circumference and area. (Note that you will need to import your Input class.)
