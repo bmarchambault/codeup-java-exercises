952675b2ba9aa1b2943b982d05e348cd0c097812
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-13 18:40:57 -0500

    created arrays exercises for curriculum

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
 
 
@@ -6,7 +8,35 @@ public class ArraysExercises {
         System.out.println(numbers);
 //        returns reference type
 
+//Create an array that holds 3 Person objects. Assign a new instance of the Person class to each element.
+
+       Person [] personArray = new Person[3];
+        personArray[0] = new Person("Carl", "Rangle");
+        personArray[1] = new Person("Monica", "Aldridge");
+        personArray[2] = new Person("Hunter", "Davis");
+
+        //Iterate through the array and print out the name of each person in the array.
+//        for(Person people: personArray){
+//            System.out.println(people.getFirstName() + " " +  people.getLastName() + ",");
+//        }
+
+        personArray = addPerson(personArray, new Person("Robert", "Engall"));//creates adds a person
+//        Person[] personArray2 = addPerson(personArray, new Person("Cathy", "Martinez"));//saves value of adding a person into a new array to preserve the original
+//        Person[] personArray3 = addPerson(personArray2, new Person("Justin", "Martinez"));//saves value of adding a person into the previous new array to preserve the previous array
+
+
+        for(Person people: personArray){
+            System.out.println(people.getFirstName() + " " +  people.getLastName() + ",");
+        }
 
 
     }
+
+    public static Person[] addPerson(Person[] personArray, Person extraPerson ){
+        Person[] addPersonArray = Arrays.copyOf(personArray, personArray.length+1);
+        addPersonArray[addPersonArray.length-1] = extraPerson;
+
+        return addPersonArray;
+    }
+
 }
