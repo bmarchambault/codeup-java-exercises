298e0c3fc36b78202382c635d95fa642436d54ee
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-18 12:06:02 -0500

    modified shapes test for shape exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -2,12 +2,14 @@ package shapes;
 
 public class ShapesTest {
     public static void main(String[] args) {
-        Rectangle box1 = new Rectangle(4, 5);
-        System.out.println(box1.getArea());
-        System.out.println(box1.getPerimeter());
+        Measurable box1 = new Rectangle(4, 5);
+        System.out.println(box1.getArea() + "rectangle");
+        System.out.println(box1.getPerimeter() + "rectangle");
+//        System.out.println(box1.getLength());//inheritance only flows one way.
+//        System.out.println(Quadrilateral.getLength);//doesn't work because getLength is not static so it belongs to the class and not an instance of Quadrilateral
 
-        Rectangle box2 = new Square(5);
-        System.out.println(box2.getArea());
-        System.out.println(box2.getPerimeter());
+        Measurable box2 = new Square(5);
+        System.out.println(box2.getArea() + "square");
+        System.out.println(box2.getPerimeter() + "square");
     }
 }
