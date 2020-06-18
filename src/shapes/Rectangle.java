9b32124476e8de77bf2e7174263df0c0e65faf41
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-18 12:05:35 -0500

    modified Rectangle class for shapes exercise

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,19 +1,68 @@
 package shapes;
 
-public class Rectangle {
-    private double length;
-    private double width;
+public class Rectangle extends Quadrilateral implements Measurable{
 
-    public Rectangle (double length, double width){
+    public Rectangle(double length, double width) {
+        super(length, width);
+    }
+
+    @Override
+    public double getLength() {
+        return super.getLength();
+    }
+
+    @Override
+    public void setLength(double length) {
         this.length = length;
+    }
+
+    @Override
+    public double getWidth() {
+        return super.getWidth();
+    }
+
+    @Override
+    public void setWidth(double width) {
         this.width = width;
     }
 
-    public double getArea(){
-        return length * width;
+    @Override
+    public double getPerimeter() {
+        return (length * 2) + (width * 2);
     }
 
-    public double getPerimeter() {
-        return ((length * 2) + (width * 2));
+    @Override
+    public double getArea() {
+        return length * width;
     }
 }
+//    More Shapes - abstract and interface exercises
+//
+//Remove the code from the body of your Rectangle and Square classes./
+// /    so commenting these out.
+//    =======================================
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle (double length, double width){
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public double getLength() { return length; }
+//
+//    public void setLength(double length) { this.length = length; }
+//
+//    public double getWidth() { return width; }
+//
+//    public void setWidth(double width) { this.width = width; }
+//
+//    public double getArea(){
+//        return length * width;
+//    }
+//
+//    public double getPerimeter() {
+//        return ((length * 2) + (width * 2));
+//    }
+////=========================
+//}
