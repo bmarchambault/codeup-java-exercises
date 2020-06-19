ca5ac41fd69c5f1bea5855b2b7b893531131b5ab
Bobbie Archambault <bmarchambault@gmail.com>
2020-06-19 17:35:13 -0500

    did grocercy list bonus"

--- src/arraysLecture/arraysBonus/ArrayBonusGroceryList.java
+++ src/arraysLecture/arraysBonus/ArrayBonusGroceryList.java
@@ -4,44 +4,44 @@ import util.Input;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
-
-public class ArrayBonusGroceryList {
-    public static void main(String[] args) {
-        Input in = new Input();
-
-
-
-        boolean makeGroceryList = in.yesNo("Would you like to create a grocery list?");
-        if (makeGroceryList == true) {
-
-            int arrayLength = in.getInt("How many items are on your list?");
-            GroceryItem[] groceryItemsArray = new GroceryItem[arrayLength];
-            System.out.println("There will be " + groceryItemsArray.length + "item's in this list.");
-            int arrayLengthCounter = 0;
-            do {
-                for ( int index = 0; index <= groceryItemsArray.length -1; index++) {
-
-                    String itemName = in.getWord("Enter an item");
-                    String itemCategory = in.getWord("What category is " + index + "?");
-                    int numOfItems = in.getInt("How many of this item do you need?");
-                    groceryItemsArray[index] = new GroceryItem(itemName, itemCategory, numOfItems);
-                    arrayLengthCounter++;
-
-                }
-            } while(arrayLengthCounter < groceryItemsArray.length);
-
-            for(GroceryItem groceryItem: groceryItemsArray){
-                System.out.println(groceryItem.getItemName() + groceryItem.getItemCategory() + groceryItem.numOfItems);
-            }
-            System.out.println(groceryItemsArray.length);
-        }
-
-//fds
-
-    }
-}
+//
+//public class ArrayBonusGroceryList {
+//    public static void main(String[] args) {
+////        Input in = new Input();
 
 
+//
+//        boolean makeGroceryList = in.yesNo("Would you like to create a grocery list?");
+//        if (makeGroceryList == true) {
+//
+//            int arrayLength = in.getInt("How many items are on your list?");
+//            GroceryItem[] groceryItemsArray = new GroceryItem[arrayLength];
+//            System.out.println("There will be " + groceryItemsArray.length + "item's in this list.");
+//            int arrayLengthCounter = 0;
+//            do {
+//                for ( int index = 0; index <= groceryItemsArray.length -1; index++) {
+//
+//                    String itemName = in.getWord("Enter an item");
+//                    String itemCategory = in.getWord("What category is " + index + "?");
+//                    int numOfItems = in.getInt("How many of this item do you need?");
+//                    groceryItemsArray[index] = new GroceryItem(itemName, itemCategory, numOfItems);
+//                    arrayLengthCounter++;
+//
+//                }
+//            } while(arrayLengthCounter < groceryItemsArray.length);
+//
+//            for(GroceryItem groceryItem: groceryItemsArray){
+//                System.out.println(groceryItem.getItemName() + groceryItem.getItemCategory() + groceryItem.numOfItems);
+//            }
+//            System.out.println(groceryItemsArray.length);
+//        }
+//
+////fds
+//
+//    }
+//}
+//
+//
 
 //}
 //1. Create a command line grocery list maker
