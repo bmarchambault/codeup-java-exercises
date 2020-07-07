e3d1236bc69df4e6bd2a61dc290c0d4b980767a1
Bobbie Archambault <bmarchambault@gmail.com>
2020-07-07 16:06:41 -0500

    created file helper for extra java practice

--- src/arraysLecture/ArraysLecture.java
+++ src/arraysLecture/ArraysLecture.java
@@ -1,55 +1,55 @@
-package arraysLecture;
-
-import java.util.Arrays;
-import java.util.ArrayList;
-
-public class ArraysLecture {
-
-    public static void main(String[] args) {
-
-//        String[] planets = new String[8];
-        //or initialize:
-        int numberOfPlanets = 8;
-        String[] planets = new String[numberOfPlanets];
-
-        System.out.println(planets.length);//prints a number
-        System.out.println(planets[0]);//zero based index.  right now, its going to say null because our array is empty.  need to assign values.
-        planets[0]= "Mercury";
-        planets[7]= "Neptune";
-//        you can just :  planets[8] = "Pluto" and expect a new index to be created because we defined the array to have 7 indexes.  we'll get a compile error saying out of bounds.
+//package arraysLecture;
 //
-//===============================================================================
-// if we know the values of the objects we can use an array initializer (array literal)
-        int[] myNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-//==================================================================================
-      for(int x: myNumbers){
-            ++x;
-            System.out.println(x);
-        }
+//import java.util.Arrays;
+//import java.util.ArrayList;
 //
-        int[] otherNumbers = {5,4,3,2,1};
-        Arrays.sort(otherNumbers);
-        System.out.println(Arrays.toString(otherNumbers));
-
-        System.out.println(Arrays.equals(myNumbers,otherNumbers));
+//public class ArraysLecture {
 //
-////        create an Array containing Person objects
-        Person justin = new Person("Justin", "Reich");
+//    public static void main(String[] args) {
 //
-        Person[] instructors = new Person[2];
-////        Person[] instructors = {new Person(), new Person(), new Person()};
-        instructors[0] = justin;
-        instructors[1] = new Person("Sophie","K");
-        System.out.println(Arrays.toString(instructors));
-
-        Person[] oneMoreInstructor = Arrays.copyOf(instructors,instructors.length + 1);
-        System.out.println(Arrays.toString(oneMoreInstructor));
-        oneMoreInstructor[2] = new Person("John","Smith");
-        System.out.println(Arrays.toString(oneMoreInstructor));
-
-        for(Person instructor : oneMoreInstructor){
-            System.out.println(instructor.getFirstName());
-        }
+////        String[] planets = new String[8];
+//        //or initialize:
+//        int numberOfPlanets = 8;
+//        String[] planets = new String[numberOfPlanets];
+//
+//        System.out.println(planets.length);//prints a number
+//        System.out.println(planets[0]);//zero based index.  right now, its going to say null because our array is empty.  need to assign values.
+//        planets[0]= "Mercury";
+//        planets[7]= "Neptune";
+////        you can just :  planets[8] = "Pluto" and expect a new index to be created because we defined the array to have 7 indexes.  we'll get a compile error saying out of bounds.
+////
+////===============================================================================
+//// if we know the values of the objects we can use an array initializer (array literal)
+//        int[] myNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+////==================================================================================
+//      for(int x: myNumbers){
+//            ++x;
+//            System.out.println(x);
+//        }
+////
+//        int[] otherNumbers = {5,4,3,2,1};
+//        Arrays.sort(otherNumbers);
+//        System.out.println(Arrays.toString(otherNumbers));
+//
+//        System.out.println(Arrays.equals(myNumbers,otherNumbers));
+////
+//////        create an Array containing Person objects
+//        Person justin = new Person("Justin", "Reich");
+////
+//        Person[] instructors = new Person[2];
+//////        Person[] instructors = {new Person(), new Person(), new Person()};
+//        instructors[0] = justin;
+//        instructors[1] = new Person("Sophie","K");
+//        System.out.println(Arrays.toString(instructors));
+//
+//        Person[] oneMoreInstructor = Arrays.copyOf(instructors,instructors.length + 1);
+//        System.out.println(Arrays.toString(oneMoreInstructor));
+//        oneMoreInstructor[2] = new Person("John","Smith");
+//        System.out.println(Arrays.toString(oneMoreInstructor));
+//
+//        for(Person instructor : oneMoreInstructor){
+//            System.out.println(instructor.getFirstName());
+//        }
 
 //
 ////        instructors[2] = new Person("David","Stephens");
@@ -59,53 +59,53 @@ public class ArraysLecture {
 //
 ////        System.out.println(instructors[2].getLastName());
 //
-////        creating an Array of Car objects
-        Car[] myCars = {new Car(), new Car(), null, null};
-////        instantiate a new Object
-        myCars[2] = new Car();
-        myCars[2].setMake("Nissan");
-        myCars[2].setModel("Rogue");
-        myCars[2].setYear(2017);
-        System.out.println(myCars[2].getMake());
-        System.out.println(myCars[2].getModel());
-        System.out.println(myCars[2].getYear());
-
-        Car marksCar = myCars[0];
-//        marksCar[0] = new Car();
-        marksCar.setMake("Ford");
-        System.out.println(marksCar.getMake());
-        System.out.println(myCars[1].getMake());
+//////        creating an Array of Car objects
+//        Car[] myCars = {new Car(), new Car(), null, null};
+//////        instantiate a new Object
+//        myCars[2] = new Car();
+//        myCars[2].setMake("Nissan");
+//        myCars[2].setModel("Rogue");
+//        myCars[2].setYear(2017);
+//        System.out.println(myCars[2].getMake());
+//        System.out.println(myCars[2].getModel());
+//        System.out.println(myCars[2].getYear());
 //
-        String[] theBeatles = {"John", "Ringo", "George", "Paul"};
-        Arrays.sort(theBeatles);
-        System.out.println(theBeatles);
-
-        for(String beatle: theBeatles){
-            System.out.println(beatle);
-        }
+//        Car marksCar = myCars[0];
+////        marksCar[0] = new Car();
+//        marksCar.setMake("Ford");
+//        System.out.println(marksCar.getMake());
+//        System.out.println(myCars[1].getMake());
+////
+//        String[] theBeatles = {"John", "Ringo", "George", "Paul"};
+//        Arrays.sort(theBeatles);
+//        System.out.println(theBeatles);
 //
-////        Array manipulation methods
-////        Arrays.fill
-        String[] colors = new String[7];
-        Arrays.fill(colors, "blue");
-////        Arrays.toString
-        System.out.println(Arrays.toString(colors));
-        System.out.println(Arrays.toString(colorsCopy(colors, "red")));
-////        Arrays.equals
-        System.out.println(Arrays.equals(instructors,theBeatles));
-    }
-//}
-
-
-
+//        for(String beatle: theBeatles){
+//            System.out.println(beatle);
+//        }
+////
+//////        Array manipulation methods
+//////        Arrays.fill
+//        String[] colors = new String[7];
+//        Arrays.fill(colors, "blue");
+//////        Arrays.toString
+//        System.out.println(Arrays.toString(colors));
+//        System.out.println(Arrays.toString(colorsCopy(colors, "red")));
+//////        Arrays.equals
+//        System.out.println(Arrays.equals(instructors,theBeatles));
 //    }
-
-    public static String[] colorsCopy(String[] array, String color) {
-        String[] newCopy = Arrays.copyOf(array, array.length + 1);
-
-        newCopy[newCopy.length - 1] = color;
-        return newCopy;
-    }
-//there was an import of arrays for this method to work
-
-}
+////}
+//
+//
+//
+////    }
+//
+//    public static String[] colorsCopy(String[] array, String color) {
+//        String[] newCopy = Arrays.copyOf(array, array.length + 1);
+//
+//        newCopy[newCopy.length - 1] = color;
+//        return newCopy;
+//    }
+////there was an import of arrays for this method to work
+//
+//}
--- src/oopLecture/oopLecture.java
+++ src/oopLecture/oopLecture.java
@@ -1,26 +1,26 @@
-package oopLecture;
-//create a package from src folder.  then create everything your gonna use with your main class in there.
-public class oopLecture {
-    public static void main(String[] args) {
-
-
-    Person rick = new Person();
-    rick.firstName = "Rick";
-    rick.lastName = "Thatguy";
-        System.out.println(rick.firstName);
-//if we don't give it a value (on like 8 and 9) it defaults to null.  if its a number type, it defaults to zero
-//even if there are hard coded values in the reference class, if you assign something different here, it'll temp. reassign it.
-//to see the reference id of the object:  sout the word after person.  sout(rick);  so if you were to create another rick and
-        //did a sout(rick == rick1), the return will be false because of the reference id.
-
-        Dog d1 = new Dog();
-        d1.age = 3;
-        d1.breed = "corgi";
-//        di.canBreed = false;
-        d1.name = "Tater";
-        d1.callForDog("Tater");
-
-
-
-}
-}
+//package oopLecture;
+////create a package from src folder.  then create everything your gonna use with your main class in there.
+//public class oopLecture {
+//    public static void main(String[] args) {
+//
+//
+//    Person rick = new Person();
+//    rick.firstName = "Rick";
+//    rick.lastName = "Thatguy";
+//        System.out.println(rick.firstName);
+////if we don't give it a value (on like 8 and 9) it defaults to null.  if its a number type, it defaults to zero
+////even if there are hard coded values in the reference class, if you assign something different here, it'll temp. reassign it.
+////to see the reference id of the object:  sout the word after person.  sout(rick);  so if you were to create another rick and
+//        //did a sout(rick == rick1), the return will be false because of the reference id.
+//
+//        Dog d1 = new Dog();
+//        d1.age = 3;
+//        d1.breed = "corgi";
+////        di.canBreed = false;
+//        d1.name = "Tater";
+//        d1.callForDog("Tater");
+//
+//
+//
+//}
+//}
