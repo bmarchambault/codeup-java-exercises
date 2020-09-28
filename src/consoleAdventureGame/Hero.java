261436e27296838a6875b1f3b6113321dd0017bc
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-27 23:19:39 -0500

    working on rpg command line game, created Hero class

--- src/consoleAdventureGame/Hero.java
+++ src/consoleAdventureGame/Hero.java
@@ -1,2 +1,40 @@
-package consoleAdventureGame;public class Hero {
+package consoleAdventureGame;
+import util.Input;
+
+public class Hero extends AdventureGame {
+    Input userInput = new Input();
+    private String herosName;
+    int herosHealth = 100;
+
+
+    public Hero() {
+        this.herosName = userInput.getString("Great, Please enter a name");
+    }
+
+
+
+    public String showInfo() {
+        return "Heros Name: " + herosName + '\n' +
+                "Heros Health = " + herosHealth;
+    }
+
+    @Override
+    public void attack() {
+        System.out.println("You decreased enemy's health by 5 points");
+    }
+
+    @Override
+    public void drinkPotion() {
+        System.out.println("You increased your health by 15 points");
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Better luck next time");
+    }
+
+    @Override
+    public void remainingHealth() {
+
+    }
 }
