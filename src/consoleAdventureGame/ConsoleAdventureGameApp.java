66d8ee76b698295cc034eb0d409490b31af86fac
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-28 15:35:58 -0500

    attack works

--- src/consoleAdventureGame/ConsoleAdventureGameApp.java
+++ src/consoleAdventureGame/ConsoleAdventureGameApp.java
@@ -1,12 +1,13 @@
 package consoleAdventureGame;
-
+import java.lang.String;
 import util.Input;
 
 public class ConsoleAdventureGameApp {
     public static void main(String[] args) {
-        Input userInput = new Input();
-       boolean confirmPlay;
 
+        Input userInput = new Input();
+        boolean confirmPlay;
+        String nextMove;
 
         //ask if the user is ready to start
         //ask user for their name
@@ -16,15 +17,25 @@ public class ConsoleAdventureGameApp {
 
         confirmPlay = userInput.yesNo("Would you like to play a role player game? ");
 //        System.out.println(playGame);
+
         if (confirmPlay) {
             Hero hero = new Hero();
-            System.out.println("What would you like to do next?  a = attack, d = drink potion, r = run");
+            hero.showInfo();
+            Enemy enemy = new Enemy();
+            enemy.showInfo();
+            nextMove = userInput.getString("What would you like to do next?  a = attack, d = drink potion, r = run");
+
+            System.out.println(nextMove);
+            if(nextMove.equalsIgnoreCase("a")){
+                hero.attack();
+               enemy.attacked();
+            }
         } else {
             System.out.println("Maybe next time");
         }
 
+
+
     }
-
-
 }
 
