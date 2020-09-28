92548f26c664e460943f77cc2df524d693e70360
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-28 15:33:56 -0500

    created methods for sub classes

--- src/consoleAdventureGame/AdventureGame.java
+++ src/consoleAdventureGame/AdventureGame.java
@@ -1,15 +1,22 @@
 package consoleAdventureGame;
 
-public abstract class AdventureGame {
+import util.Input;
 
-    int attack = 5;
-    int defend = 3;
-    int potion = 15;
-    int remainingHeath;
+public abstract class AdventureGame {
+    Input userInput = new Input();
+    protected String herosName;
+    protected int herosHealth = 100;
+    protected int enemysHealth = 75;
+    protected int attack = 5;
+   protected int defend = 3;
+    protected int potion = 15;
+    protected int remainingHeath;
+   protected boolean endGame = false;
 
     public abstract void attack();
 //    public void defend(){};
     public abstract void drinkPotion();
     public abstract void run();
     public abstract void remainingHealth ();
+    public abstract void attacked();
 }
