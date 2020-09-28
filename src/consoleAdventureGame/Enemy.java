89f8aecdafe15eb45c81c4c71bf786e597d4ac56
Bobbie Archambault <bmarchambault@gmail.com>
2020-09-27 23:19:07 -0500

    working on rpg command line game, created enemy class

--- src/consoleAdventureGame/Enemy.java
+++ src/consoleAdventureGame/Enemy.java
@@ -1,2 +1,36 @@
-package consoleAdventureGame;public class Enemy {
+package consoleAdventureGame;
+
+public class Enemy extends AdventureGame {
+    int enemyHealth = 75;
+
+    public Enemy(int enemyHealth) {
+        this.enemyHealth = enemyHealth;
+    }
+
+    public int getEnemyHealth() {
+        return enemyHealth;
+    }
+
+    public void setEnemyHealth(int enemyHealth) {
+        this.enemyHealth = enemyHealth;
+    }
+
+    @Override
+    public void attack() {
+        enemyHealth = enemyHealth - attack;
+        System.out.println(attack);
+    }
+
+
+
+
+    @Override
+    public void drinkPotion() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
